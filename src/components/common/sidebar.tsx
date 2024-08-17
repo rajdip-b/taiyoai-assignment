357c2883f0901ca3c6e5e678e1b2baf112ca70aa
rajdip-b <agentR47@gmail.com>
2024-08-17 18:50:22 +0530

    feat: Sidebar item color based upon current path

--- src/components/common/sidebar.tsx
+++ src/components/common/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const routes = [
   {
@@ -12,12 +12,16 @@ const routes = [
 ];
 
 export default function Sidebar() {
+  const location = useLocation();
+
   return (
     <div className="flex flex-col justify-between bg-light-secondary min-w-[300px] h-[94vh] py-5">
       <div className="flex flex-col">
         {routes.map((r, i) => (
           <Link
-            className="font-semilight text-dark-secondary py-2 transition-all hover:text-primary duration-200 border-l-4 border-l-transparent hover:border-l-primary px-9 ml-1 rounded-sm"
+            className={`font-semilight py-2 transition-all hover:text-primary duration-200 border-l-4 border-l-transparent hover:border-l-primary px-9 ml-1 rounded-sm ${
+              location.pathname.startsWith(r.path) ? 'text-primary' : 'text-dark-secondary'
+            }`}
             to={r.path}
             key={i}
           >
